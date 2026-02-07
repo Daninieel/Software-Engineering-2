a6fdabcc99350972969e9d8d7ae87a273987aeda
MARK DANIEL CUNTAPAY <2023mdscuntapay@live.mcl.edu.ph>
2026-02-07 09:55:11 +0800

    add animation per page

---
+++ Soft eng/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $('a:not([target="_blank"]):not([href^="#"]):not([data-no-transition])').on('click', function (e) {
+        var href = $(this).attr('href');
+
+        if (href && !href.startsWith('http') && href !== '#' && !href.startsWith('javascript')) {
+            e.preventDefault();
+
+            $('.page-content').css({
+                'opacity': '0',
+                'transform': 'translateY(-10px)',
+                'transition': 'all 0.25s ease-in'
+            });
+
+            setTimeout(function () {
+                window.location.href = href;
+            }, 250);
+        }
+    });
+});
