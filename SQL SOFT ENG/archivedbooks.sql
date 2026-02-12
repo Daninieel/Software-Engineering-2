48f664bbdb134b84145653f2258694bb801c161c
Mark Daniel S. Cuntapay <2023mdscuntapay@live.mcl.edu.ph>
2026-02-12 21:40:21 +0800

    Update archivedbooks.sql

--- SQL SOFT ENG/archivedbooks.sql
+++ SQL SOFT ENG/archivedbooks.sql
@@ -1,5 +1,4 @@
 USE `Soft Eng 2`;
-
 CREATE TABLE ArchivedBooks (
     ArchiveID INT AUTO_INCREMENT PRIMARY KEY,
     BookID INT NOT NULL,
@@ -10,6 +9,10 @@ CREATE TABLE ArchivedBooks (
     ShelfLocation VARCHAR(100),
     TotalCopies INT,
     DateArchived DATETIME DEFAULT CURRENT_TIMESTAMP,
-    ArchiveReason VARCHAR(50)
-);
+    ArchiveReason VARCHAR(50),
 
+    CONSTRAINT fk_archived_logbook
+        FOREIGN KEY (BookID) REFERENCES Logbook(BookID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
